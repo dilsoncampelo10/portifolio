e2f9cf74802e544538ae71f3a4a756d5910ed991
dilsoncampelo10 <dilsono316@gmail.com>
2023-10-15 11:42:38 -0300

    add services in area

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IService } from 'src/app/interfaces/IService';
 
 @Component({
   selector: 'app-service',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.component.scss']
 })
 export class ServiceComponent {
-    services:{}[] = [{},{},{},{},{},{}]
+    services:IService[] = [{icon:'fa fa-2x fa-laptop',title:'Sistemas',description:'Desenvolvimento de aplicações web utilizando banco de dados, sistemas de gerenciamento para negócios e etc.'},{icon:'fa-solid fa-2x fa-server',title:'Web Services',description:'Desenvolvimento de API \'s para integrações, com foco nos padrões Restful.'},{icon:'fa-solid fa-2x fa-laptop-code',title:'Sites',description:'Desenvolvimento de websites modernos e com SEO otimizados, consumo de Api\'s e mais!'},{icon:'fa-solid fa-2x fa-gears',title:'Manutenção',description:'Manter sistemas, microsserviços, sites e demais softwares que se fazem necessários para o cliente'}]
 }
+
+
