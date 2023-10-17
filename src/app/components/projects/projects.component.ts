c37abbd81f4048213ff16a83117b9e982c5e0bd8
dilsoncampelo10 <dilsono316@gmail.com>
2023-10-17 10:14:30 -0300

    add projects, part 1

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProject } from 'src/app/interfaces/IProject';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
-    projects = [{},{},{},{},{},{}]
+    projects: IProject[]= [{image:'nuarte.png',title:'Palco Nuarte'},{image:'domp.png',title:'Domp Shop'},{image:'cleanica.png',title:'Cleanica'},{image:'academy.png',title:'Spring Academy'},{image:'',title:'MyStore API'},{image:'',title:'Task API'}];
 }
